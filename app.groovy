0da0c93cb8c84bcb230573b098d337266e13fdf6
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-13 15:03:50 -0500

    Resolves #2: Import statement no longer needed

--- app.groovy
+++ app.groovy
@@ -1,8 +1,6 @@
 @Grab("org.webjars:jquery:2.0.3-1") // this will automatically fetch jquery
 @Grab("thymeleaf-spring4")
 
-import org.springframework.ui.Model
-
 @Controller
 class Application {
 	@RequestMapping("/greeting")
